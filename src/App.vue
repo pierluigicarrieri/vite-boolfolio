c8c756b4d87622eb53da3e655560ea7caae38d26
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-10-16 17:21:42 +0200

    data correctly fetched

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,42 @@
 <script>
+
+import axios from 'axios';
+
 	export default {
 
-    
+    data() {
+
+      return {
+
+        fetched_data : [],
+        pagination_info : {},
+      }
+
+    },
+
+    methods : {
+
+      fetchData() {
+
+        axios.get('http://127.0.0.1:8000/api/projects').then((response) => {
+          this.fetched_data = response.data.results.data;
+          delete response.data.results.data;
+          this.pagination_info = response.data.results;
+        });
+
+      },
+
+    },
+
+    mounted() {
+      this.fetchData()
+    }
 
 	}
 </script>
 
 <template>
-    <h1>Ciao</h1>
+    <h1>Ciao belli</h1>
 </template>
 
 <style>
