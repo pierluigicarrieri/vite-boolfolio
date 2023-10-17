2ca66fea9034f7ed7d10e9423b3717cce4410e2f
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-10-17 18:00:56 +0200

    Cleaned up some code in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,14 @@
 <script>
 
-// import axios from 'axios';
-// import ProjectCard from "./components/ProjectCard.vue";
-import NavBar from "./components/Navbar.vue";
+  import NavBar from "./components/Navbar.vue";
 
 	export default {
 
     components : {
-      // ProjectCard,
+    
       NavBar
+
     },
-
-    // data() {
-
-    //   return {
-
-    //     fetched_data : [],
-    //     pagination_info : {},
-    //   }
-
-    // },
-
-    // methods : {
-
-    //   fetchData(url) {
-
-    //     axios.get(url ?? 'http://127.0.0.1:8000/api/projects').then((response) => {
-    //       this.fetched_data = response.data.results.data;
-    //       delete response.data.results.data;
-    //       this.pagination_info = response.data.results;
-    //     });
-
-    //   },
-
-    // },
-
-    // mounted() {
-    //   this.fetchData()
-    // }
 
 	}
 </script>
@@ -47,30 +18,6 @@ import NavBar from "./components/Navbar.vue";
     <NavBar></NavBar>
 
     <router-view></router-view>
-
-    <!-- <h1 class="py-5 text-center text-danger">Questa è la parte pubblica della mia app</h1>
-
-    <div class="main">
-
-      <div class="container">
-
-        <div class="row row-cols-2 g-4">
-
-          <div class="col" v-for="project in fetched_data" :key="project.id">
-
-            <ProjectCard :project="project"></ProjectCard>
-
-          </div>
-
-        </div>
-
-        <div class="d-flex justify-content-center py-4">
-          <a class="btn btn-link" v-for="linkToPage in pagination_info.links" @click="fetchData(linkToPage.url)" v-html="linkToPage.label"></a>
-        </div>
-
-      </div>
-
-    </div> -->
 
 </template>
 
